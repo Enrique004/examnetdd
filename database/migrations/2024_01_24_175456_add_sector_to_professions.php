067bbe54cb2981ebcb916c8d6bb99ff4b33d253c
Enrique <enriquealarconjimenez1@gmail.com>
2024-01-24 17:57:40 +0000

    Ejercicio 1: Ampliar la tabla de profesiones

---
+++ database/migrations/2024_01_24_175456_add_sector_to_professions.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddSectorToProfessions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('professions', function (Blueprint $table) {
+            $table->string('sector')->after('salary');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('professions', function (Blueprint $table) {
+            //
+        });
+    }
+}
