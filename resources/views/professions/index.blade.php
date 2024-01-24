2bb149ff68e895efed5c5eb9b0655386e68fed89
Enrique <enriquealarconjimenez1@gmail.com>
2024-01-24 18:47:22 +0000

    Corregido error ejercicio 4

--- resources/views/professions/index.blade.php
+++ resources/views/professions/index.blade.php
@@ -7,43 +7,47 @@
         <h1 class="pb-1">Listado de profesiones</h1>
     </div>
 
-    <table class="table">
-        <thead class="thead-dark">
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col">Título</th>
-            <th scope="col">Nivel de educación</th>
-            <th scope="col">Salario</th>
-            <th scope="col">Experiencia requerida</th>
-            <th scope="col">Perfiles</th>
-            <th scope="col">Acciones</th>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach($professions as $profession)
-            <tr>
-                <td scope="row">{{ $profession->id }}</td>
-                <td>
-                    {{ $profession->title }}
-                    <span class="note">{{ $profession->sector }}</span>
-                </td>
-                <td>{{$profession->education_level}}</td>
-                <td>{{$profession->salary}}</td>
-                <td>{{$profession->experience_required}}</td>
-                <td>{{ $profession->profiles_count }}</td>
-                <td>
-                    @if($profession->profiles_count == 0)
-                        <form action="{{ url('profesiones/' . $profession->id) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-link"><span
-                                        class="material-symbols-outlined">delete</span>
-                            </button>
-                        </form>
-                    @endif
-                </td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
+   @if($professions->isNotEmpty())
+       <table class="table">
+           <thead class="thead-dark">
+           <tr>
+               <th scope="col">#</th>
+               <th scope="col">Título</th>
+               <th scope="col">Nivel de educación</th>
+               <th scope="col">Salario</th>
+               <th scope="col">Experiencia requerida</th>
+               <th scope="col">Perfiles</th>
+               <th scope="col">Acciones</th>
+           </tr>
+           </thead>
+           <tbody>
+           @foreach($professions as $profession)
+               <tr>
+                   <td scope="row">{{ $profession->id }}</td>
+                   <td>
+                       {{ $profession->title }}
+                       <span class="note">{{ $profession->sector }}</span>
+                   </td>
+                   <td>{{$profession->education_level}}</td>
+                   <td>{{$profession->salary}}</td>
+                   <td>{{$profession->experience_required}}</td>
+                   <td>{{ $profession->profiles_count }}</td>
+                   <td>
+                       @if($profession->profiles_count == 0)
+                           <form action="{{ url('profesiones/' . $profession->id) }}" method="POST">
+                               @csrf
+                               @method('DELETE')
+                               <button type="submit" class="btn btn-link"><span
+                                           class="material-symbols-outlined">delete</span>
+                               </button>
+                           </form>
+                       @endif
+                   </td>
+               </tr>
+           @endforeach
+           </tbody>
+       </table>
+   @else
+       No hay profesiones
+   @endif
 @endsection
