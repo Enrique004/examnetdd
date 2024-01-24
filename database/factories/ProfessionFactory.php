87dde63ee431bd013fc89d3fcb39824000492390
Enrique <enriquealarconjimenez1@gmail.com>
2024-01-24 18:35:12 +0000

    Parte ejercicio 3: modificar factoria

--- database/factories/ProfessionFactory.php
+++ database/factories/ProfessionFactory.php
@@ -5,5 +5,10 @@ use Faker\Generator as Faker;
 $factory->define(App\Profession::class, function (Faker $faker) {
     return [
         'title' => $faker->sentence(3),
+        'description' => $faker->text(),
+        'education_level' => $faker->sentence(3),
+        'salary' => $faker->numberBetween(0,5000),
+        'sector' => $faker->sentence(3),
+        'experience_required' => $faker->numberBetween(0,6)
     ];
 });
